70547fc01567c73be048b4705ee80c2f767a33fa
Никитина Юля <nik.iuliiasw@gmail.com>
2023-01-10 12:52:57 +0100

    updated url link to google cloud

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,4 +1,4 @@
 export const API_URL = (slug) =>
-  `https://julia-elsa-fullstack.herokuapp.com/${slug}`
+  `https://project-auth-43tusg3yva-lz.a.run.app/${slug}`
 export const NASA_URL =
   'https://api.nasa.gov/planetary/apod?api_key=cV42g4DJQAPOKbVzIiw5DwGUcbWirgP8DKHsM2Z4'
